a526131ce9959fec8ae8e739f252c67f71b8f117
luckstrike <alexisgarc711@gmail.com>
2020-09-06 01:32:22 -0500

    Started MongoDB setup

--- flask_src/app.py
+++ flask_src/app.py
@@ -1,8 +1,18 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, jsonify
+import pymongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
+cluster = MongoClient("mongodb+srv://bacon:tamushamu2001@cluster0.3ksm9.mongodb.net/howdy-hack-2020?retryWrites=true&w=majority")
+db = cluster["howdy-hack-2020"]
+collection = db["howdy-hack-2020"]
 
-@app.route("/", methods=["POST", "GET"])
+post = {"_id":00, "email": "test@gmail.com", "phone": 1234567890}
+
+# inserts a post/ document into the mongodb collection
+collection.insert_one(post)
+
+@app.route("/", methods=["POST", "SGET"])
 def breach():
     if request.method == "POST":
         user = request.form["nm"]
